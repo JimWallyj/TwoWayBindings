353c6fe9319468320e36ebb85e2ccbe7c71f62ce
Jim Walejko <jwalejko@me.com>
2025-12-09 22:16:53 -0700

    Prof. Gallauger Programming Course, Chapter 1: Slider Control & Labels, View showing slider values, Decimal precision = Int(), formatted(.number.precision(.fractionLength(value))), format specifiers / format strings

--- TwoWayBindings/ContentView.swift
+++ TwoWayBindings/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
     @State private var selectedColor: Color = .red
     @State private var selectedDate = Date()
     @State private var stepperValue = 1
+    @State private var sliderValue = 50.0
     
     var body: some View {
         VStack {
@@ -50,6 +51,23 @@ struct ContentView: View {
             Spacer()
             
             Stepper("Stepper value: \(stepperValue)", value: $stepperValue, in: 1...10)
+            
+            Spacer()
+            
+            //Slider(value: $sliderValue, in: 0...100)
+            Slider(value: $sliderValue, in: 0...100) {
+                //Accessibility label
+            } minimumValueLabel: {
+                Image(systemName: "speaker.minus")
+               // Text("0")
+            } maximumValueLabel: {
+                Image(systemName: "speaker.plus")
+               // Text("100")
+            }
+
+            Text("Slider value: \(Int(sliderValue))%")
+            //Text("Slider value: \(sliderValue.formatted(.number.precision(.fractionLength(1))))")
+            //Text("Slider value: \(String(format: "%.0f", sliderValue))")
             
             Spacer()
         }
